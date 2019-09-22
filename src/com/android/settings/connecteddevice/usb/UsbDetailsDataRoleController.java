2eb967b9fdf86b53a9d1cc27268fdd0b66efb53b
Thespartann <danilastefan69@yahoo.ro>
2019-09-22 23:13:10 +0300

    Settings: Remove hide unsupported USB modes feature
    This reverts commit 9f3d55dfd9d2ea9a615f45a3e5c7c7854cf46d57.

--- src/com/android/settings/connecteddevice/usb/UsbDetailsDataRoleController.java
+++ src/com/android/settings/connecteddevice/usb/UsbDetailsDataRoleController.java
@@ -113,8 +113,7 @@ public class UsbDetailsDataRoleController extends UsbDetailsController
 
     @Override
     public boolean isAvailable() {
-        return !Utils.isMonkeyRunning()
-                && !mUsbBackend.isSingleDataRoleSupported();
+        return !Utils.isMonkeyRunning();
     }
 
     @Override
