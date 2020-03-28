19e54c56434c9da4e4ac7745e90145f6103ff087
HeroBuxx <herobuxx@gmail.com>
2020-03-28 21:09:17 +0700

    Fix
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Change-Id: I13d02fd6d81253663887bd23e4a294c777554ea9

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -285,7 +285,7 @@ public class SettingsGateway {
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
-            DarkModeSettingsFragment.class.getName()
+            DarkModeSettingsFragment.class.getName(),
             ControlPanel.class.getName()
     };
 
